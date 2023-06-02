a66f01f87d2ef00238cc402889488ebc78ab36ea
pacman-admin <getpacman.gq@gmail.com>
2023-06-02 15:42:06 -0400

    Add files

--- pac_man.java
+++ pac_man.java
@@ -10,7 +10,7 @@ import processing.sound.SoundFile;
 public class pac_man extends PApplet {
     // By Langdon S.
     //current version:
-    final float version = 10.22f;
+    final float version = 10.23f;
 //      
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|
 //            G A M E  S E T T I N G S            |
